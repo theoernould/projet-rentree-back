19dc6755af20f96488989966f39cd37e309faf82
Théo Ernould <theo.ernould@outlook.fr>
2023-08-30 09:29:17 +0200

    is working

--- src/main/java/imt/projetrentree/projet/config/JerseyConfig.java
+++ src/main/java/imt/projetrentree/projet/config/JerseyConfig.java
@@ -1,11 +1,9 @@
 package imt.projetrentree.projet.config;
 
-import imt.projetrentree.projet.controllers.MainController;
 import jakarta.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
-import javax.servlet.ServletConfig;
 
 @Component
 @Configuration
@@ -17,15 +15,11 @@ public class JerseyConfig extends ResourceConfig {
 
         // Assurez-vous de gérer correctement les erreurs de conversion
         // ça marche pas
-       int jerseyPort = 8081; // Port par défaut si la valeur du port n'est pas valide
+        int jerseyPort = 8081; // Port par défaut si la valeur du port n'est pas valide
 
         // Définissez le port pour Jersey
         property("jersey.config.server.port", jerseyPort);
 
-        // Utilisez getResource pour obtenir le chemin relatif à votre classe JerseyConfig
-        String basePackage = JerseyConfig.class.getPackage().getName();
-
-        // Ajoutez le chemin relatif à votre package de contrôleurs
-        packages(basePackage + ".controllers");
+        packages("imt.projetrentree.projet.controllers");
     }
 }
