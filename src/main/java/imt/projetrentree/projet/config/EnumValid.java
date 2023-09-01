3cf8b3537bd7bd621365c29a04171f670025dc10
Théo Ernould <theo.ernould@outlook.fr>
2023-09-01 11:44:11 +0200

    filters working

---
+++ src/main/java/imt/projetrentree/projet/config/EnumValid.java
@@ -0,0 +1,17 @@
+package imt.projetrentree.projet.config;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = EnumValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.TYPE_USE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EnumValid {
+    String message() default "Invalid enum value";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    Class<? extends Enum<?>> enumClass();
+}
