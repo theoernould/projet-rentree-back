0168b91e50e25e9108129337d3b5cde5620505f3
Cyrildemand <cydemand@gmail.com>
2023-09-19 16:09:59 +0200

    changement cors

--- src/main/java/imt/projetrentree/projet/filters/CORSFilter.java
+++ src/main/java/imt/projetrentree/projet/filters/CORSFilter.java
@@ -14,7 +14,7 @@ public class CORSFilter implements ContainerResponseFilter {
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
             throws IOException {
 
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:19006");
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, token, content-type, accept, authorization");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD");
